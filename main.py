1001fa438fa1185263733e2f816f3d9330af2123
DISK131 <dada.00131@gmail.com>
2021-08-29 16:40:28 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from logger import set_logger
+
+logger = set_logger()
+
+
+def main():
+    logger.info("============infoメッセージ============")
+    logger.warning("============warningメッセージ============")
+    logger.error("============errorメッセージ============")
+
+
+if __name__ == "__main__":
+    main()
