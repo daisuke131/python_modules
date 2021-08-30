8f4829ec80756b7fe77b20daea3a0377ad21b726
DISK131 <dada.00131@gmail.com>
2021-08-30 09:50:49 +0900

    Update beutifulsoup.py

--- common/beutifulsoup.py
+++ common/beutifulsoup.py
@@ -4,7 +4,17 @@ from bs4 import BeautifulSoup
 from common.util import fetch_user_agent
 
 
-def fetch_soup(url: str):
-    headers = {"User-Agent": fetch_user_agent()}
-    resp = requests.get(url, headers=headers)
-    return BeautifulSoup(resp.text, "html.parser")
+class Soup:
+    def __init__(self, url: str) -> None:
+        self.soup = self.fetch_soup(url)
+
+    def fetch_soup(self, url: str):
+        headers = {"User-Agent": fetch_user_agent()}
+        resp = requests.get(url, headers=headers)
+        return BeautifulSoup(resp.text, "html.parser")
+
+    def selects(self, selector: str):
+        return self.soup.select(selector)
+
+    def select(self, selector: str):
+        return self.soup.select_one(selector)
