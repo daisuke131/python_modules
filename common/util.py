7e3a40a4061c0237bf19d11ab581e1aecffaf583
DISK131 <dada.00131@gmail.com>
2021-09-02 15:29:24 +0900

    Update util.py

--- common/util.py
+++ common/util.py
@@ -10,21 +10,27 @@ def hyphen_now():
 
 
 def fetch_user_agent() -> str:
-    load_dotenv()
-    # user_agent = [
-    #     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    #     + "(KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    #     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
-    #     + "(KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
-    #     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    #     + "(KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
-    #     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    #     + "(KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-    # ]
-    # return user_agent[random.randrange(0, len(user_agent), 1)]
-    return os.getenv("UA_AGENT")
+    user_agent = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        + "(KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
+        + "(KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        + "(KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        + "(KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+    ]
+    return user_agent[random.randrange(0, len(user_agent), 1)]
 
 
 def filename_creation(filename: str) -> str:
-    os.path.join(os.getcwd(), "csv")
     return "{filename}_{datetime}".format(filename=filename, datetime=hyphen_now())
+
+
+def fetch_env(env_key: str) -> str:
+    load_dotenv()
+    return os.getenv(env_key)
+
+
+def fetch_absolute_path(folder_name: str) -> str:
+    return os.path.join(os.getcwd(), folder_name)
