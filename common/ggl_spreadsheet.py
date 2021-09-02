b3a16480852fb438c90da67cabde978f80024f65
DISK131 <dada.00131@gmail.com>
2021-09-02 15:28:54 +0900

    Update ggl_spreadsheet.py

--- common/ggl_spreadsheet.py
+++ common/ggl_spreadsheet.py
@@ -1,38 +1,88 @@
-import os
-
 import gspread
 import pandas as pd
-from dotenv import load_dotenv
-from gspread.models import Worksheet
+from gspread.models import Spreadsheet, Worksheet
 from oauth2client.service_account import ServiceAccountCredentials
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
-JASON_FILE_NAME = "python-buyable-twitter-bot-0bb63e591ec9.json"
-JSON_PATH = os.path.join(os.getcwd(), JASON_FILE_NAME)
-load_dotenv()
-SPREAD_SHEET_KEY = os.getenv("SPREAD_SHEET_KEY")
+from common.util import fetch_absolute_path, fetch_env
+
+JASON_FILE_NAME = fetch_env("JASON_FILE_NAME")
+JSON_PATH = fetch_absolute_path(JASON_FILE_NAME)
+SPREAD_SHEET_KEY = fetch_env("SPREAD_SHEET_KEY")
 
 
 class Gspread:
     def __init__(self) -> None:
-        self.workbook = self.connect_gspread()
+        self.workbook: Spreadsheet
         self.worksheet: Worksheet
+        self.drive: GoogleDrive
+        self.credentials = self.fetch_credentials()
+        # self.folder_name: str
+        self.new_folder_id: str
         self.df = []
+
+    def fetch_credentials(self):
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive",
+        ]
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            JASON_FILE_NAME, scope
+        )
+        return credentials
+
+    def create_folder(self, folder_name: str) -> None:
+        gauth = GoogleAuth()
+        gauth.credentials = self.credentials
+        self.drive = GoogleDrive(gauth)
+        folder_id = "1aRDY5vSQRVz3hXlWrPr1xHEtv4Ilijz3"
+        f_folder = self.drive.CreateFile(
+            {
+                "title": folder_name,
+                "parents": [{"id": folder_id}],
+                "mimeType": "application/vnd.google-apps.folder",
+            }
+        )
+        f_folder.Upload()
+        self.new_folder_id = self.drive.ListFile(
+            {"q": f'title = "{folder_name}"'}
+        ).GetList()[0]["id"]
+
+    def create_spreadsheet(self, file_name: str) -> None:
+        f = self.drive.CreateFile(
+            {
+                "title": file_name,
+                "mimeType": "application/vnd.google-apps.spreadsheet",
+                "parents": [{"id": self.new_folder_id}],
+            }
+        )
+        f.Upload()
+        gc = gspread.authorize(self.credentials)
+        # ワークシート指定
+        self.workbook = gc.open_by_key(f["id"])
+        self.worksheet = self.workbook.sheet1
+
+    def save_file(self, local_img_path: str, file_name: str) -> None:
+        f = self.drive.CreateFile({"parents": [{"id": self.new_folder_id}]})
+        f.SetContentFile(local_img_path + "/" + file_name)
+        f["title"] = file_name
+        f.Upload()
+
+    def update_cell(self, row: int, column: int, val: str) -> None:
+        self.worksheet.update_cell(row, column, val)
+
+    def append_row(self, val: list) -> None:
+        self.worksheet.append_row(val)
 
     def connect_gspread(self):
         try:
-            scope = [
-                "https://spreadsheets.google.com/feeds",
-                "https://www.googleapis.com/auth/drive",
-            ]
-            credentials = ServiceAccountCredentials.from_json_keyfile_name(
-                JSON_PATH, scope
-            )
-            gc = gspread.authorize(credentials)
+            gc = gspread.authorize(self.credentials)
             workbook = gc.open_by_key(SPREAD_SHEET_KEY)
             return workbook
         except Exception:
             print("Googleスプレッドシートを読み込めませんでした。")
-            # return None
+            return None
 
     def read_sheet(self, sheet_num: int):
         # 0番目が一枚目のシート
