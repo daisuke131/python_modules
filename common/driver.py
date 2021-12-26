dd206d7792ba762c63d0f2b62181fccc84c0ca8a
DISK131 <dada.00131@gmail.com>
2021-12-26 21:35:56 +0900

    Update driver.py

--- common/driver.py
+++ common/driver.py
@@ -1,6 +1,8 @@
 import os
+from subprocess import CREATE_NO_WINDOW
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.utils import ChromeType
@@ -43,21 +45,16 @@ class Driver:
         options.add_argument("--lang=ja")
         # 画像を読み込まないで軽くする
         # options.add_argument("--blink-settings=imagesEnabled=false")
+        service = Service(ChromeDriverManager().install())
+        service.creationflags = CREATE_NO_WINDOW
 
         try:
             if "firefox" in BROWSER_NAME:
-                driver = webdriver.Firefox(
-                    executable_path=GeckoDriverManager().install(), options=options
-                )
+                driver = webdriver.Firefox(service=service, options=options)
             elif "chromium" in BROWSER_NAME:
-                driver = webdriver.Chrome(
-                    ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),
-                    options=options,
-                )
+                driver = webdriver.Chrome(service=service, options=options)
             else:
-                driver = webdriver.Chrome(
-                    ChromeDriverManager().install(), options=options
-                )
+                driver = webdriver.Chrome(service=service, options=options)
             return driver
         except Exception:
             return None
